109727433c7b2728cc86b185f27ae06b557e8238
Richard Walker <digitalsadhu@gmail.com>
2016-10-22 21:13:34 +0200

    Add tests and implementation

--- test/default.spec.js
+++ test/default.spec.js
@@ -1,7 +1,25 @@
 /* global describe, it */
-var assert = require('assert')
-describe('the module', function () {
-  it('should should be ok', function () {
-    assert.ok(true)
+const assert = require('assert')
+const FileNotFoundError = require('../src')
+
+describe('FileNotFoundError', function () {
+  it('name and path', function () {
+    const error = new FileNotFoundError('index.js', '/src/project/')
+    assert.strictEqual(error.message, 'Unable to find file index.js in /src/project/')
+  })
+
+  it('name only', function () {
+    const error = new FileNotFoundError('/src/project/index.js')
+    assert.strictEqual(error.message, 'Unable to find file /src/project/index.js')
+  })
+
+  it('name must be a string', function () {
+    const test = () => new FileNotFoundError(1)
+    assert.throws(test)
+  })
+
+  it('path must be a string if present', function () {
+    const test = () => new FileNotFoundError('asd', 1)
+    assert.throws(test)
   })
 })
